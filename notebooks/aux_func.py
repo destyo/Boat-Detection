666002de8d0ed4cdd656655be4f83d11e84b9695
Your Name <atello1998@gmail.com>
2022-05-03 19:59:32 +0200

    final version

--- notebooks/aux_func.py
+++ notebooks/aux_func.py
@@ -1,4 +1,9 @@
 import os
+import matplotlib.pyplot as plt
+from tensorflow.python.keras.callbacks import LearningRateScheduler
+import tensorflow as tf
+from tensorflow.keras import backend as K
+
 
 def search_names(path, extension='.csv'):
     """
@@ -10,3 +15,55 @@ def search_names(path, extension='.csv'):
             if filename.endswith(extension):
                 matches.append(os.path.join(root, filename))
     return matches
+
+
+
+def show_history(history, metrics=['accuracy']):
+    """
+    Visualize history
+    :param history:
+    :return:
+    """
+
+    # Plot history: Loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Loss history')
+    plt.ylabel('Loss value')
+    plt.xlabel('No. epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+    for metric in metrics:
+        # Plot history:
+        plt.plot(history.history[metric])
+        plt.plot(history.history['val_' + metric])
+        plt.title(metric + ' history')
+        plt.ylabel(metric + ' value (%)')
+        plt.xlabel('No. epoch')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
+
+
+def step_decay(epochs_drop):
+    def step_decay_fun(epoch):
+        initial_lrate = 0.001
+        drop = 0.000001
+        lrate = initial_lrate * tf.math.pow(drop, tf.math.floor((1+epoch)/epochs_drop))
+        return lrate
+    return step_decay_fun
+
+
+def r2_keras(y_true, y_pred):
+    """
+    Custom R2-score metrics for keras backend
+    :param y_true: valor real
+    :type y_true: Tensor
+    :param y_pred: valor predicho
+    :type y_pred: Tensor
+    :return: r2 score
+    :rtype: tensor
+    """
+    ss_res = K.sum(K.square(y_true - y_pred))
+    ss_tot = K.sum(K.square(y_true - K.mean(y_true)))
+    return 1 - ss_res / (ss_tot + K.epsilon())
